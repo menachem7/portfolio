1bc76ff305aa140b26b7fd4d02ea1ce377e3fdeb
Menachem Polonsky <m@mbenleonardo.com>
2019-01-17 10:07:11 -0500

    new commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+  document.getElementById("menuOverlay").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("menuOverlay").style.height = "0%";
+}
