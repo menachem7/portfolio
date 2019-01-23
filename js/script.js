a344659b9d7510abe9cf51a9c022878ef80abbb2
Menachem Polonsky <m@mbenleonardo.com>
2019-01-22 17:47:57 -0700

    Portfolio Update - Functionality and Design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// Close Home
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    document.getElementById("home").style.height = "0%";
+  }
+}
+
+// Open Home
+
+function openHome() {
+  document.getElementById("home").style.height = "100%";
+}
+
+// Menu Overlay
+
 /* Open */
 function openNav() {
   document.getElementById("menuOverlay").style.height = "100%";
@@ -6,4 +24,73 @@ function openNav() {
 /* Close */
 function closeNav() {
   document.getElementById("menuOverlay").style.height = "0%";
+  document.getElementById("click").innerHTML = "click to copy";
 }
+
+// Select Contact
+
+function selectContact() {
+  document.getElementById("click").style.color = "#6AEBF7";
+}
+
+function deselectContact() {
+  document.getElementById("click").style.color = "#567582";
+}
+
+// Copy Contact
+
+function copiedConfirmation() {
+  document.getElementById("click").innerHTML = "copied!";
+}
+
+// Image Slider
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+//
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+
+var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+} 
